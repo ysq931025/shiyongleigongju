742aabca2fdc1b98926bdd5ed4bb99c8de4ffee3
jinjinyike@163.com <jinjinyike@163.com>
2018-01-11 17:15:47 +0800

    canvas图片合成form表单上传图片formdata图片上传

---
+++ tupianhechengjiaformshangchuanheyulan/upload.php
@@ -0,0 +1,10 @@
+<meta charset="UTF-8">
+<?php
+//header("Content-Type: application/json; charset=UTF-8");
+$photo=$_POST['photo'];
+$name=$_POST['username'];
+var_dump($photo);
+var_dump($name);
+
+
+?>
